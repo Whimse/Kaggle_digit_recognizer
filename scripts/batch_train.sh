889c614948d8f8f3faaefc6e1bc40351f8a88e03
Antonio L <antonio.l.rodriguez.lopez@gmail.com>
2022-05-14 14:24:06 +0200

    Minor change

--- scripts/batch_train.sh
+++ scripts/batch_train.sh
@@ -2,9 +2,6 @@
 
 NETS='resnet18 resnet34 mobilenet_v2 mobilenet_v3_small mobilenet_v3_large efficientnet_b0 efficientnet_b3 squeezenet1_0 squeezenet1_1 mnasnet0_5 mnasnet0_75 mnasnet1_0 mnasnet1_3'
 
-# Clean experiments
-rm -rf mlruns models
-
 # Run one experiment at a time (requires task spooler installed)
 tsp -S 1 -K
 
